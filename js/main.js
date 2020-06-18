03eccf06f376df058a6c54bc4a6b2deb4e91e60f
Gabriel <gabrielrosabueno@hotmail.com>
2020-06-18 09:35:40 -0300

    Noções de Var, Arrays e Console

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+/*var nome = "Gabriel ";
+var idade = 18;
+alert("Seja bem vindo ao johnson, " + nome + idade);
+
+console.log(nome);
+console.log (idade);
+
+var lista = ["maçã", "pera", "laranja"];
+lista.push ("uva");
+
+console.log (lista);
+console.log (lista.toString())
+*/
+
+var frutas = [{nome: "maçã", cor: "verde"}, {nome:"laranja", cor: "laranja"}]
+console.log (frutas);
+alert(frutas [1].nome);
